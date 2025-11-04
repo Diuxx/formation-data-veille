e391120bf95b525cc6956ed1c142a2a7dcef5fd7
Diuxx <nicolasmarmot@gmail.com>
2025-11-04 18:15:27 -0300

    feat(shutdown, pino): add gracefull shutdown and use logging outside middleware

--- api/src/routes/features/health/health.route.js
+++ api/src/routes/features/health/health.route.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import logger from '../../../middlewares/logger.js';
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-  res.status(200).json({ status: 'ok' });
+router.get('/',  async (_req, res) => {
+  try {
+    logger.info('displaying api status (test)')
+    res.status(200).json({ status: 'ok' });
+  }
+  catch (err) {
+    next(err);
+  }
 });
 
 export default router;
--- api/src/server.js
+++ api/src/server.js
@@ -5,6 +5,18 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+const shutdown = () => {
+  console.log('Stopping server...');
+  server.close(() => {
+    console.log('Server closed.');
+    process.exit(0);
+  });
+};
+
+// signals
+process.on('SIGINT', shutdown); // Ctrl + C.
+process.on('SIGTERM', shutdown); // kill or Docker stop.
