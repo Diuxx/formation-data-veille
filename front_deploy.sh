53c43c3e5281a37b06f98ab2d5a2f356765a15e3
Diuxx <nicolasmarmot@gmail.com>
2026-03-01 17:32:59 +0100

    feat(deploy): update Angular build directory and enhance SSL certificate handling in Nginx configuration

--- front_deploy.sh
+++ front_deploy.sh
@@ -8,7 +8,7 @@ PROJECT_DIR="$(pwd)"
 # Angular variables
 ANGULAR_DIR="web"
 ANGULAR_WWW_DIR="/var/www/verly"
-ANGULAR_BUILD_DIR="$PROJECT_DIR/$ANGULAR_DIR/dist"
+ANGULAR_BUILD_DIR="$PROJECT_DIR/$ANGULAR_DIR/dist/verly/browser"
 ANGULAR_NGINX_CONF_SRC="$PROJECT_DIR/infra/nginx/verly_front.conf"
 ANGULAR_NGINX_CONF_DST="/etc/nginx/sites-available/verly_front.conf"
 ANGULAR_NGINX_ENABLED="/etc/nginx/sites-enabled/verly_front.conf"
@@ -47,19 +47,19 @@ sudo nginx -t
 echo "♻️ Reloading Nginx..."
 sudo systemctl reload nginx
 
-# echo "Applying SSL certificate with Certbot..."
-# if [ ! -d "/etc/letsencrypt/live/$DOMAIN" ]; then
-#   echo "🔐 No SSL certificate found, generating one..."
+echo "Applying SSL certificate with Certbot..."
+if [ ! -d "/etc/letsencrypt/live/$DOMAIN" ]; then
+  echo "🔐 No SSL certificate found, generating one..."
 
-#   sudo certbot --nginx \
-#     -d "$DOMAIN" \
-#     -d "www.$DOMAIN" \
-#     --non-interactive \
-#     --agree-tos \
-#     --email "$EMAIL" \
-#     --redirect
-# else
-#   echo "🔒 SSL certificate already exists"
-# fi
+  sudo certbot --nginx \
+    -d "$DOMAIN" \
+    -d "www.$DOMAIN" \
+    --non-interactive \
+    --agree-tos \
+    --email "$EMAIL" \
+    --redirect
+else
+  echo "🔒 SSL certificate already exists"
+fi
 
 echo "✅ Angular deployment complete!"
